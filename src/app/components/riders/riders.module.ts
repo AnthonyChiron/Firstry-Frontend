9c9d8297c748287ee7f9be9980e5c1a7b0c24a1f
Anthony CHIRON <74792178+AnthonyChiron@users.noreply.github.com>
2023-09-25 21:23:29 +0200

    Début de la fiche rider

--- src/app/components/riders/riders.module.ts
+++ src/app/components/riders/riders.module.ts
@@ -1,12 +1,12 @@
 import { SharedModule } from 'src/app/shared/shared.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RiderCardComponent } from './rider-card/rider-card.component';
-import { HttpClientModule } from '@angular/common/http';
+import { RiderCardMediumComponent } from './rider-card-md/rider-card-md.component';
 import { RouterModule, Routes } from '@angular/router';
 import { RidersComponent } from './riders.component';
 import { RidersService } from 'src/app/shared/data/RidersService/riders.service';
 import { RiderComponent } from './rider/rider.component';
+import { RiderCardLargeComponent } from './rider-card-large/rider-card-large.component';
 
 const ridersRoutes: Routes = [
   { path: '', component: RidersComponent },
@@ -14,7 +14,12 @@ const ridersRoutes: Routes = [
 ];
 
 @NgModule({
-  declarations: [RidersComponent, RiderCardComponent, RiderComponent],
+  declarations: [
+    RidersComponent,
+    RiderCardMediumComponent,
+    RiderCardLargeComponent,
+    RiderComponent,
+  ],
   imports: [CommonModule, RouterModule.forChild(ridersRoutes), SharedModule],
   providers: [RidersService],
 })
---
+++ src/app/shared/components/quadrillage/quadrillage.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'quadrillage',
+  templateUrl: './quadrillage.component.html',
+  styleUrls: ['./quadrillage.component.scss'],
+})
+export class QuadrillageComponent {
+  @Input() nbRows: number = 0;
+  @Input() nbColumns: number = 0;
+  @Input() width: number = 0;
+  @Input() heigth: number = 0;
+}
