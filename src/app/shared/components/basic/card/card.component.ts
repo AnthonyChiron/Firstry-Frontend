fc3f295fa85b8175e32bdcd02044cbdf8b68f9cd
Anthony CHIRON <74792178+AnthonyChiron@users.noreply.github.com>
2023-10-26 19:17:40 +0200

    Ajout de bouton personnalisé sur une carte - Début de l'édition des infos contest

--- src/app/shared/components/basic/card/card.component.ts
+++ src/app/shared/components/basic/card/card.component.ts
@@ -1,5 +1,5 @@
 import { ToggleButtonModule } from 'primeng/togglebutton';
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'card',
@@ -11,5 +11,12 @@ export class CardComponent {
   @Input() close: boolean = false;
   @Input() redirect: boolean = false;
   @Input() redirectLink: string = '';
+  @Input() btn: boolean = false;
+  @Input() btnIcon: string = '';
+  @Output() btnClicked: EventEmitter<any> = new EventEmitter();
   toggle: boolean = false;
+
+  emitBtnClicked() {
+    this.btnClicked.emit();
+  }
 }
