3bb72cb70c91645fd269c83719b90e61b72e300f
Anthony CHIRON <74792178+AnthonyChiron@users.noreply.github.com>
2023-12-16 00:08:57 +0100

    Correction du bug css de planning en production - Ajout de la modification de l'url api en fonction du build prod

--- src/app/shared/components/basic/planning/planning.component.ts
+++ src/app/shared/components/basic/planning/planning.component.ts
@@ -11,7 +11,10 @@ import { StepModelDTO, stepModelToDTO } from 'src/app/models/step.model';
 @Component({
   selector: 'planning',
   templateUrl: './planning.component.html',
-  styleUrls: ['./planning.component.scss'],
+  styleUrls: [
+    '../../../../../../node_modules/angular-calendar/css/angular-calendar.css',
+    './planning.component.scss',
+  ],
 })
 export class PlanningComponent implements OnInit, OnChanges {
   @Input() categories: any[] = [];
