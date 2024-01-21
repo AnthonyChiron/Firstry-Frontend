184335a030ed83599068d77dab3ea140ba93f589
Anthony CHIRON <74792178+AnthonyChiron@users.noreply.github.com>
2024-01-21 13:16:16 +0100

    Test sans animation

--- src/app/components/accueil/accueil.component.ts
+++ src/app/components/accueil/accueil.component.ts
@@ -1,25 +1,11 @@
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/shared/services/AuthService/auth.service';
-import {
-  animate,
-  state,
-  style,
-  transition,
-  trigger,
-} from '@angular/animations';
 
 @Component({
   selector: 'app-accueil',
   templateUrl: './accueil.component.html',
   styleUrls: ['./accueil.component.scss'],
-  animations: [
-    trigger('fadeInOut', [
-      state('void', style({ opacity: 0 })),
-      transition('void <=> *', animate('500ms ease-in-out')),
-      transition('* <=> void', animate('500ms ease-in-out')),
-    ]),
-  ],
 })
 export class AccueilComponent implements OnInit {
   user;
