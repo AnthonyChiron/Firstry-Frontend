b4699e7e7d31d4ad79c47ca38ab0b01bdc9264f4
Anthony CHIRON <74792178+AnthonyChiron@users.noreply.github.com>
2024-01-23 16:40:07 +0100

    Gestion des poules avec enregistrement sur le back

--- src/app/components/dashboard/riders/pools-handler/pools-handler.component.ts
+++ src/app/components/dashboard/riders/pools-handler/pools-handler.component.ts
@@ -1,4 +1,10 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 import { CategoryModel } from 'src/app/models/category.model';
 import { RegistrationModel } from 'src/app/models/registration.model';
 import { RiderModel } from 'src/app/models/rider.model';
@@ -7,18 +13,26 @@ import {
   moveItemInArray,
   transferArrayItem,
 } from '@angular/cdk/drag-drop';
+import { PoolsService } from 'src/app/shared/data/PoolsService/pools.service';
 
 @Component({
   selector: 'pools-handler',
   templateUrl: './pools-handler.component.html',
   styleUrls: ['./pools-handler.component.scss'],
 })
-export class PoolsHandlerComponent implements OnChanges {
+export class PoolsHandlerComponent implements OnInit, OnChanges {
   @Input() category: CategoryModel;
   @Input() registrations: RegistrationModel[];
+  isNewPools: boolean = true;
+  originalPools: any;
   pools: any[][] = [];
   ridersPerPool: number = 3;
-  poolsIds = ['pool-0', 'pool-1', 'pool-2', 'pool-3', 'pool-4', 'pool-5'];
+  poolsIds = [];
+  currentStepId: string;
+  edit: boolean = false;
+  isLoading: boolean = false;
+  isError: boolean = false;
+  errorMessage: string = '';
 
   perPoolOptions = [
     { label: '1', value: 1 },
@@ -27,16 +41,32 @@ export class PoolsHandlerComponent implements OnChanges {
     { label: '4', value: 4 },
     { label: '5', value: 5 },
     { label: '6', value: 6 },
+    { label: '7', value: 7 },
   ];
 
-  ngOnChanges(changes: SimpleChanges): void {
-    console.log(changes);
-    if (this.registrations) this.createPools(this.registrations);
+  constructor(private _poolsService: PoolsService) {}
+
+  async ngOnInit() {
+    this.isLoading = true;
+    this.currentStepId = this.category.steps[0]._id;
+    await this.getPoolFromDb();
+  }
+
+  async ngOnChanges(changes: SimpleChanges) {
+    this.pools = [];
+    this.edit = false;
+    this.currentStepId = this.category.steps[0]._id;
+    await this.getPoolFromDb();
+
+    console.log(this.isNewPools);
+    console.log(this.registrations);
+    if (this.registrations && this.isNewPools)
+      this.formatPoolsFromRegistrations(this.registrations);
   }
 
   selectPerPool(event) {
     this.ridersPerPool = event;
-    this.createPools(this.registrations);
+    this.formatPoolsFromRegistrations(this.registrations);
   }
 
   updatePoolsIds() {
@@ -44,6 +74,7 @@ export class PoolsHandlerComponent implements OnChanges {
   }
 
   drop(event: CdkDragDrop<any>) {
+    console.log(event.container.data);
     if (event.previousContainer === event.container) {
       moveItemInArray(
         event.container.data,
@@ -60,10 +91,14 @@ export class PoolsHandlerComponent implements OnChanges {
     }
   }
 
-  createPools(registrations: RegistrationModel[]) {
-    console.log(registrations);
+  formatPoolsFromRegistrations(registrations: RegistrationModel[]): void {
+    const riders = registrations.map((registration) => registration);
 
-    const riders = registrations.map((registration) => registration.rider);
+    if (riders.length === 0) return;
+    if (riders.length < this.ridersPerPool) {
+      this.pools = [[...riders]];
+      return;
+    }
     let nbPools = Math.ceil(riders.length / this.ridersPerPool);
     const nbRiderLeft = riders.length % this.ridersPerPool;
 
@@ -86,11 +121,97 @@ export class PoolsHandlerComponent implements OnChanges {
       );
     }
 
-    console.log(pools);
-
     this.pools = [...pools];
     this.updatePoolsIds(); // Mise à jour des identifiants si nécessaire
+  }
 
-    return pools;
+  formatPoolsFromDb(pools: any[]) {
+    this.pools = pools.reduce((acc, pool) => {
+      const poolNumber = pool.poolNumber - 1;
+      if (!acc[poolNumber]) acc[poolNumber] = [];
+      acc[poolNumber].push(pool.registration);
+
+      return acc;
+    }, []);
+    console.log(this.pools);
+  }
+
+  cancel() {
+    this.edit = false;
+    this.isError = false;
+
+    if (this.isNewPools) this.formatPoolsFromRegistrations(this.registrations);
+    else this.formatPoolsFromDb(this.originalPools);
+  }
+
+  submit() {
+    // Parcourir les pools et créer un tableau avec numéro de pool, registrationId
+    const pools = [];
+
+    // Check si une pool est vide
+    for (let i = 0; i < this.pools.length; i++) {
+      if (this.pools[i].length === 0) {
+        this.isError = true;
+        this.errorMessage = `La poule ${
+          i + 1
+        } est vide. Vous ne pouvez pas soumettre de poules vides.`;
+        this.isLoading = false;
+        return;
+      }
+    }
+
+    this.edit = false;
+    this.isLoading = true;
+
+    this.pools.forEach((pool, index) => {
+      pool.forEach((rider) => {
+        pools.push({
+          poolNumber: index + 1,
+          registrationId: rider._id,
+          stepId: this.currentStepId,
+        });
+      });
+    });
+
+    if (this.isNewPools)
+      this._poolsService
+        .createPools(this.currentStepId, pools)
+        .subscribe((pools) => {
+          console.log(pools);
+
+          this.originalPools = [...pools];
+          this.isNewPools = false;
+          this.isLoading = false;
+        });
+    else
+      this._poolsService
+        .updatePools(this.currentStepId, pools)
+        .subscribe((pools) => {
+          console.log(pools);
+
+          this.originalPools = [...pools];
+          this.isNewPools = false;
+          this.isLoading = false;
+        });
+  }
+
+  async getPoolFromDb() {
+    this._poolsService
+      .getPoolsByStepId(this.currentStepId)
+      .subscribe((pools: any) => {
+        this.originalPools = [...pools];
+        console.log(pools);
+
+        if (pools.length > 0) {
+          this.pools = pools.map((pool) => pool.riders);
+          this.isNewPools = false;
+          this.formatPoolsFromDb(pools);
+          this.updatePoolsIds();
+        } else {
+          this.isNewPools = true;
+        }
+
+        this.isLoading = false;
+      });
   }
 }
--- src/app/shared/data/PoolsService/pools.service.ts
+++ src/app/shared/data/PoolsService/pools.service.ts
@@ -11,4 +11,28 @@ export class PoolsService extends CrudService<PoolModel> {
   constructor(http: HttpClient, authService: AuthService) {
     super(http, authService, 'pools');
   }
+
+  getPoolsByStepId(stepId: string) {
+    return this.http.get<PoolModel[]>(
+      `${this.baseUrl}/getPoolsByStepId/${stepId}`
+    );
+  }
+
+  createPools(stepId: string, pools: any[]) {
+    return this.http.post<PoolModel[]>(
+      `${this.baseUrl}/createPoolsByStepId/${stepId}`,
+      {
+        poolsEntries: pools,
+      }
+    );
+  }
+
+  updatePools(stepId: string, pools: any[]) {
+    return this.http.post<PoolModel[]>(
+      `${this.baseUrl}/updatePoolsByStepId/${stepId}`,
+      {
+        poolsEntries: pools,
+      }
+    );
+  }
 }
