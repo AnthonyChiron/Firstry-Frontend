13923de5fcc8b809428a7d8a57e912dda1dab718
Anthony CHIRON <74792178+AnthonyChiron@users.noreply.github.com>
2024-02-09 13:08:23 +0100

    https

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  backendUrl: 'http://178.16.131.148:4443/api/',
+  backendUrl: 'https://firstry.fr:4443/api/',
   stripe_public_key:
     'pk_test_51OPhx3ExeV2TEn3k0EWYu7LkqusSy8cewkqOMeV6ydwt6ICp84mIxzw2oPzyh8v3awLSP9ymlJqrx2ysjS00TKlU00yuzgNMzN',
   firebaseConfig: {
